e90c8b0e02e4fe47cc6be72dd32b3e5ae514f9c4
Daniel Mitchell <daniel.mitchell2008@gmail.com>
2025-08-14 15:39:07 -0500

    operacion matematica compleja

--- archivotaller2.py
+++ archivotaller2.py
@@ -1,2 +1,8 @@
 print("Hola, este es mi segundo commit en Git")  # línea modificada
 print("Ahora agregué una línea nueva para el punto 10 del taller")  # línea agregada
+import numpy as np
+uno = 1
+dos = 2
+cuatro = 4
+multi = dos * cuatro
+print (multi)
